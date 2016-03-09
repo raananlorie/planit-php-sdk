c9f19fe53af5604851d87f09015dfe63073c4643
David Meir-Levy <davidmeirlevy@gmail.com>
2016-03-09 10:20:12 +0200

    request method

--- planit.php
+++ planit.php
@@ -3,11 +3,14 @@
 namespace Planit;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 
 class API {
   private $api;
   private $token;
   private $client;
+  private $auth_promise;
+  private $auth_result;
   
   function __construct($secretKey, $api) {
       if ((!isset($secretKey)) || (!$secretKey)) {
@@ -17,6 +20,22 @@ class API {
       $this->api = $api ? $api : 'https://plan-it-com.herokuapp.com';
       $this->token = $secretKey;
       $this->client = new \GuzzleHttp\Client(['cookies' => true, 'base_uri' => $this->api]);
-      $client->requestAsync('POST', 'auth/token', ['body' => ['token' => $this->token]]);
+      $this->auth_promise = $this->client->requestAsync('POST', 'auth/token', ['body' => ['token' => $this->token]]);
+      $this->auth_promise->then(function() {
+        $this->auth_result = true;
+      }, function() {
+        $this->auth_result = false;
+      });
+  }
+  
+  private function is_auth() {
+    if(isset($this->auth_result)) return $this->auth_result;
+    $this->auth_promise->wait();
+    return $this->auth_result;
+  }
+  
+  private function request($uri, $method = 'GET', $headers) {
+    if(!$this->is_auth()) return false;
+    return $this->client->request($method, $uri, $headers);
   }
 }
