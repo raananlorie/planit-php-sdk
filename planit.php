f1f3479b2ff182ffc0ee108d268247e06ef7c4b9
David Meir-Levy <davidmeirlevy@gmail.com>
2016-03-09 14:58:02 +0200

    working api

--- planit.php
+++ planit.php
@@ -11,21 +11,22 @@ class API {
   private $client;
   private $auth_promise;
   private $auth_result;
-  
+    
   function __construct($secretKey, $api) {
-      if ((!isset($secretKey)) || (!$secretKey)) {
-          throw new Exception("no secret key");
-      }
-      
-      $this->api = $api ? $api : 'https://plan-it-com.herokuapp.com';
-      $this->token = $secretKey;
-      $this->client = new \GuzzleHttp\Client(['cookies' => true, 'base_uri' => $this->api]);
-      $this->auth_promise = $this->client->requestAsync('POST', 'auth/token', ['form_params' => ['token' => $this->token]]);
-      $this->auth_promise->then(function() {
-        $this->auth_result = true;
-      }, function() {
-        $this->auth_result = false;
-      });
+	if ((!isset($secretKey)) || (!$secretKey)) {
+	  throw new Exception("no secret key");
+	}
+
+	$this->api = $api ? $api : 'https://plan-it-com.herokuapp.com';
+	$this->token = $secretKey;
+	$this->client = new \GuzzleHttp\Client(['cookies' => true, 'base_uri' => $this->api, 'http_errors' => false]);
+	  
+	$this->auth_promise = $this->client->requestAsync('POST', 'auth/token', ['form_params' => ['token' => $this->token]]);
+	$this->auth_promise->then(function($r) {
+		$this->auth_result = ($r->getStatusCode() == 200);
+	  }, function() {
+		$this->auth_result = false;
+	});
   }
   
   private function is_auth() {
@@ -34,8 +35,56 @@ class API {
     return $this->auth_result;
   }
   
-  private function request($uri, $method = 'GET', $headers) {
+  private function request($uri, $method = 'GET', $headers = array()) {
     if(!$this->is_auth()) return false;
-    return $this->client->request($method, $uri, $headers);
+    return $this->client->request($method, $uri, $headers)->getBody()->getContents();
   }
+  
+  private function get($resource, $params = null) {
+	  if(isset($params)) {
+		  if(is_string($params)) {
+			  $resource = $resource . '/' .$params;
+		  } else {
+			  $query = (is_array($params)) ? $params : [];
+		  }
+	  } else {
+		  $query = null;
+	  }
+	  return $this->request($resource, 'GET', ['query' => $query]);
+  }
+  
+  private function create($resource, $body, $query = array()) {
+	  return $this->request($resource, 'POST', [
+		  'query' => $query,
+		  'form_params' => $body
+	  ]);
+  }
+  
+  private function update($resource, $id, $body, $query = array()) {
+	  return $this->request($resource . '/' . $id, 'PUT', [
+		  'query' => $query,
+		  'form_params' => $body
+	  ]);
+  }
+  
+  private function remove($resource, $id) {
+	  return $this->request($resource . '/' . $id, 'DELETE');
+  }
+  
+  // API
+  public function getTodos() {
+	  return $this->get('todos');
+  } 
+  public function createTodo($note = '', $isDone = false, $client = null) {
+	  $obj = [
+		'note' => $note,
+		'isDone' => $isDone
+	  ];
+	  if(isset($client)) $obj['client'] = $clientId;
+	  return $this->create('todos', $obj);
+  }
+  public function updateTodo($obj) {
+	  return $this->update('todos', $obj['id'], $obj);
+  }
+  
 }
